df67b4e62575996540650fbe26cde790001833b4
Namoral Thy <thynamoral18@gmail.com>
2024-11-07 16:10:11 +0700

    add format money,date function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatMoney(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+}
+
+export function formatDate(from: Date) {
+  return formatDistanceToNowStrict(from, {
+    addSuffix: true,
+  });
 }
